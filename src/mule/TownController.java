29f29426f1d3f2ede358f04eefb85fea84841de9
madijuby <mleejuby@gmail.com>
2015-09-24 18:12:23 -0400

    Merge branch 'master' into Madi
    Conflicts:
    	.gradle/2.6/taskArtifacts/cache.properties
    	.gradle/2.6/taskArtifacts/cache.properties.lock
    	.gradle/2.6/taskArtifacts/fileHashes.bin
    	.gradle/2.6/taskArtifacts/outputFileStates.bin
    	.gradle/2.6/taskArtifacts/taskArtifacts.bin

--- src/mule/TownController.java
+++ src/mule/TownController.java
@@ -13,21 +13,24 @@ import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class TownController {
-	
+
 	@FXML
     private Button returnToMap;
 
+	public Stage gameStage;
+
+	public Scene gameScene;
+
 	@FXML
 	private void handleReturnToMap(ActionEvent event) throws IOException {
-		Parent gameScreenParent = FXMLLoader.load(getClass().getResource("Game.fxml"));
-		Scene gameScene = new Scene(gameScreenParent);
-		Stage gameStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		gameScene = ConfigureController.gameScene;
+		gameStage = ConfigureController.gameStage;
 		gameStage.setScene(gameScene);
-		gameStage.show();
 	}
 
 	@FXML
 	private void handleEndTurn(MouseEvent event) throws IOException {
-
+		Game game = ConfigureController.game;
+		game.update();
 	}
 }
