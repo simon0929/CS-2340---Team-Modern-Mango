27b7f69c9dff73d5f098abe5e7f96f341c29cb0b
Hunter Morrell <hunter.james.morrell@gmail.com>
2015-09-30 20:26:47 -0400

    Completed my M5 requirements
    * At start of round, Decide on the player turn order by score
    * For each player, their turn time is computed
    * Timer is started at beginning of turn
    * Players turn ends if time runs out

--- src/mule/GameController.java
+++ src/mule/GameController.java
@@ -1,10 +1,11 @@
 package mule;
 
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
-import com.sun.org.apache.bcel.internal.generic.ARRAYLENGTH;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -42,11 +43,13 @@ public class GameController {
 	private Player currentPlayer;
 
     //numOfPropBuyInRound = number of properties bought this round, if 0 then land selection phase ends
-	private int turnNumber, numOfPropBuyInRound;
+	private int turnNumber, numOfPropBuyInRound, turnTime, maxTurnTime, defaultTurnTime;
 
 	private boolean selectionPhase;
 
 	public static ArrayList<Player> playerList;
+
+    private ArrayList<Pane> propertyOwnedList;
 
 	@FXML
 	private void initialize() {
@@ -65,6 +68,10 @@ public class GameController {
 		player3score.setText("0");
 		player4score.setText("0");
         game = ConfigureController.game;
+        turnTime = 0;
+        defaultTurnTime = 50;
+        updateTurnTime();
+        propertyOwnedList = new ArrayList<>();
 	}
 
     //Calls the Game.fxml file and actually constructs the GUI.
@@ -83,7 +90,7 @@ public class GameController {
         //refreshes screen
 		game.update();
 
-		round.setText(String.valueOf(this.game.getRound()));
+		round.setText(String.valueOf(game.getRound()));
 		turnNumber++;
 
         //if the number of turns exceeds the number of players, the round ends
@@ -106,7 +113,8 @@ public class GameController {
 			}
 		}
 
-		currentPlayer = ConfigureController.playerList.get(turnNumber - 1);
+        currentPlayer = playerList.get(turnNumber - 1);
+        updateTurnTime();
 
 		turn.setText(currentPlayer.getName());
 		food.setText(String.valueOf(currentPlayer.getFood()));
@@ -115,20 +123,57 @@ public class GameController {
 		ore.setText(String.valueOf(currentPlayer.getOre()));
 	}
 
+    private void handleEndTurn() {
+        //refreshes screen
+        game.update();
+
+        round.setText(String.valueOf(game.getRound()));
+        turnNumber++;
+
+        //if the number of turns exceeds the number of players, the round ends
+        if (turnNumber > ConfigureController.maxPlayers) {
+            //Calculates all player scores and rearranges playerList for a new order for the next turn
+            getTurnOrder();
+
+            //Refreshes scores on GUI
+            refreshScores();
+
+            //Resets the turn to 1
+            turnNumber = 1;
+
+
+            if (numOfPropBuyInRound == 0) {
+                selectionPhase = false;
+            }
+            else {
+                numOfPropBuyInRound = 0;
+            }
+        }
+
+        currentPlayer = playerList.get(turnNumber - 1);
+        updateTurnTime();
+
+        turn.setText(currentPlayer.getName());
+        food.setText(String.valueOf(currentPlayer.getFood()));
+        money.setText(String.valueOf(currentPlayer.getMoney()));
+        energy.setText(String.valueOf(currentPlayer.getEnergy()));
+        ore.setText(String.valueOf(currentPlayer.getOre()));
+    }
+
 	@FXML
 	private void handleProperty(MouseEvent event) throws IOException {
 		if (selectionPhase) {
-			//game = ConfigureController.game;
-
 			if (!game.selectedProp()) {
 				Color playerColor = game.getCurrPlayer().getColor();
 
-				Property property = (Property) event.getSource();
+                //Gets the actual property object that was clicked so that things can be done to it
+				Pane property = (Pane) event.getSource();
 
-				if (property.getOwner() == null || property.getOwner() == game.getCurrPlayer()) {
+				if (!propertyOwnedList.contains(property)) {
 					currentPlayer.incrementPropertyOwned();
 
 					if (currentPlayer.getNumOfFreeProperties() == 0) {
+                        //Automatically updates player's money and on the GUI
 						currentPlayer.setMoney(currentPlayer.getMoney() - 300);
                         money.setText(String.valueOf(currentPlayer.getMoney()));
 					}
@@ -137,12 +182,14 @@ public class GameController {
 					}
 
 					numOfPropBuyInRound++;
-					property.setOwner(game.getCurrPlayer());
+                    currentPlayer.addToPropertyList(property);
 
 					Region reg = (Region) event.getSource();
-					reg.setBorder(new Border(new BorderStroke(playerColor, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(8.0))));
-					//game.setSelectedProp(true);
-				}
+					reg.setBorder(new Border(new BorderStroke(playerColor, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(4.0))));
+
+                    currentPlayer.calculateScore();
+                    refreshScores();
+                }
 			}
 		}
 	}
@@ -170,7 +217,70 @@ public class GameController {
     private void refreshScores() {
         player1score.setText(String.valueOf(ConfigureController.player1.getScore()));
         player2score.setText(String.valueOf(ConfigureController.player2.getScore()));
-        player3score.setText(String.valueOf(ConfigureController.player3.getScore()));
-        player4score.setText(String.valueOf(ConfigureController.player4.getScore()));
+        if (ConfigureController.player3 != null) {
+            player3score.setText(String.valueOf(ConfigureController.player3.getScore()));
+        }
+        if (ConfigureController.player4 != null) {
+            player4score.setText(String.valueOf(ConfigureController.player4.getScore()));
+        }
+    }
+
+    private void updateTurnTime() {
+        if (game.getRound() == 1) {
+            turnTime = 50;
+        }
+        else {
+            if (game.getRound() < 5) {
+                if (currentPlayer.getFood() >= 3) {
+                    turnTime = 50;
+                }
+                else if (currentPlayer.getFood() > 0 && currentPlayer.getFood() < 3) {
+                    turnTime = 30;
+                }
+                else {
+                    turnTime = 5;
+                }
+            }
+            else if (game.getRound() < 9) {
+                if (currentPlayer.getFood() >= 4) {
+                    turnTime = 50;
+                }
+                else if (currentPlayer.getFood() > 0 && currentPlayer.getFood() < 4) {
+                    turnTime = 30;
+                }
+                else {
+                    turnTime = 5;
+                }
+            }
+            else {
+                if (currentPlayer.getFood() >= 5) {
+                    turnTime = 50;
+                }
+                else if (currentPlayer.getFood() > 0 && currentPlayer.getFood() < 5) {
+                    turnTime = 30;
+                }
+                else {
+                    turnTime = 5;
+                }
+            }
+        }
+
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                Platform.runLater(new Runnable() {
+                      @Override
+                      public void run() {
+                          if (turnTime >= 0) {
+                              timeLeft.setText(String.valueOf(turnTime--));
+                          }
+                          else {
+                              handleEndTurn();
+                          }
+                      }
+                  });
+            }
+        }, 1000, 1000);
     }
 }
