c1ea76f5caa3d11f6c3281e5fe146acf95a68396
madijuby <mleejuby@gmail.com>
2015-11-10 15:44:48 -0500

    cleanup

--- src/mule/model/FoodMule.java
+++ src/mule/model/FoodMule.java
@@ -1,12 +1,12 @@
 package mule.model;
 
-import java.util.HashMap;
 public class FoodMule extends Mule implements java.io.Serializable {
 
     /**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+    private static final int PLAIN_VALUE = 2, RIVER_VALUE = 4, M1_VALUE = 1, M2_VALUE = 1, M3_VALUE = 1;
 
     public FoodMule(String pType) {
         propertyType = pType;
@@ -15,23 +15,16 @@ public class FoodMule extends Mule implements java.io.Serializable {
     }
     static {
 
-        plainValue = 2;
-        riverValue = 4;
-        m1Value = 1;
-        m2Value = 1;
-        m3Value = 1;
-
-        productionValues = new HashMap<>(NUM_TYPES_OF_PROPERTY);
-        productionValues.put("River", riverValue);
-        productionValues.put("Plain", plainValue);
-        productionValues.put("M1", m1Value);
-        productionValues.put("M2", m2Value);
-        productionValues.put("M3", m3Value);
+        PRODUCTION_VALUES.put("River", RIVER_VALUE);
+        PRODUCTION_VALUES.put("Plain", PLAIN_VALUE);
+        PRODUCTION_VALUES.put("M1", M1_VALUE);
+        PRODUCTION_VALUES.put("M2", M2_VALUE);
+        PRODUCTION_VALUES.put("M3", M3_VALUE);
     }
 
     public final void calculateResourceChanges() {
         currentPlayer.setEnergy(currentPlayer.getEnergy() - 1);
-        currentPlayer.setFood(currentPlayer.getEnergy() + productionValues.get(propertyType));
+        currentPlayer.setFood(currentPlayer.getEnergy() + PRODUCTION_VALUES.get(propertyType));
     }
 
 }
