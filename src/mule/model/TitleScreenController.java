031f52ab3d415e2ab388ae4dd28e43c5ba2ce086
madijuby <mleejuby@gmail.com>
2015-11-14 22:17:00 -0500

    Configure screen

--- src/mule/model/TitleScreenController.java
+++ src/mule/model/TitleScreenController.java
@@ -7,29 +7,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
 import java.io.IOException;
 
 public class TitleScreenController {
-
-    @FXML
-    private Rectangle rectangle;
-
-    @FXML
-    private Button startButton1, startButton2;
-
-
-    @FXML
-    private void initialize() {
-        rectangle.setStyle("-fx-stroke-width: 5; -fx-stroke: #FFF9E2; -fx-stroke-dash-array: 10 10 10 10;");
-        startButton1.setStyle("-fx-background-color: #FFF9E2");
-        startButton2.setStyle("-fx-background-color: TRANSPARENT");
-    }
 
     @FXML
     private void handleStartGame(ActionEvent event) throws IOException {
